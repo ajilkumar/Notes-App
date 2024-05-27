4a5075d4bcb3927b92a2323fd756700d98ff104c
ajilkumar <ajilk0325@gmail.com>
2024-05-27 16:03:40 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require(dotenv).config()
+
+//essential packages
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+//express
+const app = express()
+
+//specify port
+const port = 5000 || process.env.PORT
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+//Static files
+app.use(express.static('public'))
+
+//Template engine
+app.use(expressLayouts)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+  res.render("index")
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+})
