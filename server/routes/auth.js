59fc91c3c79a061e9cab76bac864797ee780f050
ajilkumar <ajilk0325@gmail.com>
2024-06-01 11:50:01 +0530

    passport google authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const router = express.Router()
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      const newUser = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        profileImage: profile.photos[0].value,
+      };
+
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          done(null, user);
+        } else {
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  )
+);
+
+
 
 module.exports = router;
