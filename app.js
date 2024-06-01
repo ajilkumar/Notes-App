1ce1a72ca442eb29b851a47d3e3b0a9aff70c57e
ajilkumar <ajilk0325@gmail.com>
2024-06-01 11:24:53 +0530

    authentication

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ require('dotenv').config()
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const connectDB = require('./server/config/db')
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 //express
 const app = express()
 
 //specify port
 const port = 5000 || process.env.PORT
+
+app.use(passport.initialize())
+app.use(express.json())
+
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -26,6 +33,7 @@ app.set('layout', './layouts/main')
 app.set('view engine', 'ejs')
 
 //Routes
+app.use('/', require('./server/routes/auth'))
 app.use('/', require('./server/routes/index'))
 app.use('/', require('./server/routes/dashboard'))
 
