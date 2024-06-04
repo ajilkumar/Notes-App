1ff7f64362204734d36b8c01820741a4fb6d93dc
ajilkumar <ajilk0325@gmail.com>
2024-06-04 12:32:43 +0530

    notes dahsboard page updated

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -5,25 +5,72 @@ const mongoose = require('mongoose');
 //Get Dashboard
 exports.dashboard = async(req, res) => {
 
-  try {
-    await Note.insertMany([
-      {
-        user: "",
-        title: 'NodeJS Tutorial',
-        body: 'lfjdgo'
-        
-      }
-    ])
-  } catch (error) {
-    
-  }
-
-
-
   const locals = {
     title: 'DashBoard',
     description: "Free NodeJS Notes App"
   }
+
+  try {
+    // Mongoose "^7.0.0 Update
+    const notes = await Note.aggregate([
+      { $sort: { updatedAt: -1 } },
+      { $match: { user: mongoose.Types.ObjectId(req.user.id) } },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 100] },
+        },
+      }
+      ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec(); 
+
+    const count = await Note.count();
+
+    res.render('dashboard/index', {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard",
+      current: page,
+      pages: Math.ceil(count / perPage)
+    });
+ 
+    // Original Code
+    // Note.aggregate([
+    //   { $sort: { updatedAt: -1 } },
+    //   { $match: { user: mongoose.Types.ObjectId(req.user.id) } },
+    //   {
+    //     $project: {
+    //       title: { $substr: ["$title", 0, 30] },
+    //       body: { $substr: ["$body", 0, 100] },
+    //     },
+    //   },
+    // ])
+    //   .skip(perPage * page - perPage)
+    //   .limit(perPage)
+    //   .exec(function (err, notes) {
+    //     Note.count().exec(function (err, count) {
+    //       if (err) return next(err);
+    //       res.render("dashboard/index", {
+    //         userName: req.user.firstName,
+    //         locals,
+    //         notes,
+    //         layout: "../views/layouts/dashboard",
+    //         current: page,
+    //         pages: Math.ceil(count / perPage),
+    //       });
+    //     });
+    //   });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+
+
+
   res.render('dashboard/index', {
     userName: req.user.firstName,
     locals,
