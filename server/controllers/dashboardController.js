d56f5db3bcbcd2dafabb7089882da5472fa13da1
ajilkumar <ajilk0325@gmail.com>
2024-06-04 12:26:50 +0530

    notes dahsboard page

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -4,11 +4,28 @@ const mongoose = require('mongoose');
 
 //Get Dashboard
 exports.dashboard = async(req, res) => {
+
+  try {
+    await Note.insertMany([
+      {
+        user: "",
+        title: 'NodeJS Tutorial',
+        body: 'lfjdgo'
+        
+      }
+    ])
+  } catch (error) {
+    
+  }
+
+
+
   const locals = {
     title: 'DashBoard',
     description: "Free NodeJS Notes App"
   }
   res.render('dashboard/index', {
+    userName: req.user.firstName,
     locals,
     layout: '../views/layouts/dashboard'
   })
